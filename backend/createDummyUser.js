10f84c08426d618f63ba664a83d345037e4b325e
Dong <nomad1025@gmail.com>
2024-08-02 20:57:32 +0900

    Update createDummyUser.js script for generating test users with additional attributes

--- backend/createDummyUser.js
+++ backend/createDummyUser.js
@@ -1,37 +1,4 @@
-// require('dotenv').config();
-// const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt');
-// const User = require('./models/User');
-
-// const mongoURI = process.env.MONGO_URI;
-
-// mongoose.connect(mongoURI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-//   .then(async () => {
-//     console.log('MongoDB connected');
-    
-//     // Hash the password before saving
-//     const hashedPassword = await bcrypt.hash('testpassword', 10);
-    
-//     const dummyUser = new User({
-//       username: 'validUsername', // Change to your desired username
-//       password: hashedPassword, // Save the hashed password
-//     });
-    
-//     await dummyUser.save();
-//     console.log('Dummy user created:', dummyUser);
-//     mongoose.connection.close();
-//   })
-//   .catch(err => {
-//     console.error('MongoDB connection error:', err);
-//     mongoose.connection.close();
-//   });
-
-
-  //after jest testing
-  require('dotenv').config();
+require('dotenv').config();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const User = require('./models/User');
@@ -39,8 +6,8 @@ const User = require('./models/User');
 const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/my-task-manager';
 
 const dummyUserConfig = {
-  username: 'validUsername', // Temporary username for development
-  password: 'testpassword'   // Temporary password for development
+  username: 'validUsername',
+  password: 'testpassword'  
 };
 
 mongoose.connect(mongoURI, {
@@ -50,7 +17,6 @@ mongoose.connect(mongoURI, {
   .then(async () => {
     console.log('MongoDB connected');
 
-    // Hash the password before saving
     const hashedPassword = await bcrypt.hash(dummyUserConfig.password, 10);
 
     const dummyUser = new User({
