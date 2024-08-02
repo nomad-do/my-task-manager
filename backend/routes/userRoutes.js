e10d2c46a8e995a98824a16116aa636a9dade3f8
Dong <nomad1025@gmail.com>
2024-08-02 20:58:09 +0900

    Revise userRoutes.js to incorporate new user-related API routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,10 +3,8 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const authenticateToken = require('../middlewares/authenticateToken');
 
-// Route to get user profile
 router.get('/profile/:userId', authenticateToken, userController.getProfile);
 
-// Route to update user profile
 router.put('/profile/:userId', authenticateToken, userController.updateProfile);
 
 module.exports = router;
