aed70af8585cfa4c9bde583dbbeb79c16580e4de
Dong <nomad1025@gmail.com>
2024-08-24 22:36:00 +0900

    Updated forms with controlled inputs and autocomplete attributes for security and usability improvements

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,39 +1,10 @@
-// import React from 'react';
-// import { Routes, Route, Navigate } from 'react-router-dom';
-// import AuthForm from './components/AuthForm';
-// import TaskManager from './components/TaskManager';
-// import EditProfile from './components/EditProfile';
-// import Dashboard from './components/Dashboard';
-
-// const AppRoutes = ({ token, handleLogout, setToken }) => {
-//   return (
-//     <Routes>
-//       <Route path="/login" element={<AuthForm setToken={setToken} />} />
-//       <Route path="/dashboard" element={token ? <Dashboard /> : <Navigate to="/login" />} />
-//       <Route
-//         path="/tasks"
-//         element={token ? <TaskManager onLogout={handleLogout} /> : <Navigate to="/login" />}
-//       />
-//       <Route
-//         path="/edit-profile/:userId"
-//         element={token ? <EditProfile /> : <Navigate to="/login" />}
-//       />
-//       <Route path="/" element={<Navigate to="/login" />} />
-//     </Routes>
-//   );
-// };
-
-// export default AppRoutes;
-
-
-// sanitizeInput (8/20)
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import AuthForm from './components/AuthForm';
 import TaskManager from './components/TaskManager';
 import EditProfile from './components/EditProfile';
 import Dashboard from './components/Dashboard';
-import SanitizeInput from './components/SanitizeInput'; // Import SanitizeInput
+import SanitizeInput from './components/SanitizeInput';
 
 const AppRoutes = ({ token, handleLogout, setToken }) => {
   return (
@@ -48,7 +19,7 @@ const AppRoutes = ({ token, handleLogout, setToken }) => {
         path="/edit-profile/:userId"
         element={token ? <EditProfile /> : <Navigate to="/login" />}
       />
-      <Route path="/sanitize" element={<SanitizeInput />} /> {/* Add route for SanitizeInput */}
+      <Route path="/sanitize" element={<SanitizeInput />} /> 
       <Route path="/" element={<Navigate to="/login" />} />
     </Routes>
   );
